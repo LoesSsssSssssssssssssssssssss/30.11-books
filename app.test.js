32ee0e5503c92d96bd62a6ee734040674c04e6ee
Loes_s <103633598+LoesSsssSssssssssssssssssssss@users.noreply.github.com>
2022-12-25 14:19:47 +0300

    24.12

--- app.test.js
+++ app.test.js
@@ -1,30 +1,78 @@
-const request = require("supertest");
-const { server } = require("./app");
+const server = require("./app.js");
+const chaiHttp =  require("chai-http");
+const { resetWatchers } = require('nodemon/lib/monitor/watch');
+const expect = require('expect').default
+const request = require('supertest')
 
-describe("/book", () => {
-	// 1
-	it("наш тест работает", (done) => {
-		done();
-	});
+let jsonfile = require('jsonfile');
+let file = jsonfile.readFileSync('data.json');
+
+describe('/GET book', () => {
+    it('should GET all the books', done => {
+        request(server)
+            .get("/book")
+            .expect(200)
+            .end(function(err, res) {
+              if (err) throw err;
+              done();
+            });
+    })
+})
+
+describe('/POST book', () => {
+    it('should POST one book', done => {
+        const book = {
+          id: file.length,
+          amount: 1,
+          name: "Name-of-book",
+          author: "Author"
+        }
+
+        request(server)
+            .post('/book')
+            .send(book)
+            .expect(200)
+            .expect((res) => {
+                expect((res.body).length).toStrictEqual(file.length + 1)
+            })
+            .end((err, res) => {
+                if (err) return done(err)
+                done()
+            });
+    })
 });
 
-it("ответ 200", (done) => {
-	request(server) // 3
-		.get("/book")
-		.set("Accept", "application/json")
-		// .send({ text })
-		.expect(200)
-		.end((err, res) => {
-			if (err) {
-				return done(err);
-			}
-		});
+describe("/GET book :id", () => {
+    it('should GET book by id', done => {
+        request(server)
+            .get('/book/0')
+            .send(JSON.stringify(file[0], null, '\t'))
+            .expect(200)
+            .expect((res) => {
+                console.log(res.body)
+                expect((res.body).id).toStrictEqual(3)
+            })
+            .end((err, res) => {
+                if (err) return done(err)
+                done()
+            })
+    })
+})
 
-	done();
-});
-
-describe("Error", () => {
-	it("should return NotFound with status 404", function (done) {
-		request(server).get("/error").expect(404).expect("Cannot GET /error").end(done);
-	});
-});
+describe("/PUT book :id", () => {
+    const book = {
+        name: "HP",
+        author: "KK",
+    }
+    it('should update book by PUT', done => {
+        request(server)
+            .put('/api/book/2')
+            .send(book)
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.a('object');
+                res.body.should.have.property('message').eql("book has been edited")
+                done()
+            })
+    })
+})
